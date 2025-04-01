19baf59421745248bdf1484b063214fa1acc2804
yusuffaseher <yusuffaseher68@gmail.com>
2025-04-02 00:49:49 +0530

    project completed

---
+++ app.py
@@ -0,0 +1,60 @@
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+app = Flask(__name__)
+UPLOAD_FOLDER = 'static/uploads'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Configure SQLite database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///gallery.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# Database model
+class Image(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    filename = db.Column(db.String(200), nullable=False)
+
+# Create database tables
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
+def index():
+    images = Image.query.all()
+    return render_template('index.html', images=images)
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    if 'file' not in request.files:
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        return redirect(request.url)
+    if file:
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        file.save(filepath)
+        new_image = Image(filename=file.filename)
+        db.session.add(new_image)
+        db.session.commit()
+        return redirect(url_for('index'))
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+@app.route('/delete/<int:image_id>', methods=['POST'])
+def delete_file(image_id):
+    image = Image.query.get(image_id)
+    if image:
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], image.filename)
+        if os.path.exists(filepath):
+            os.remove(filepath)
+        db.session.delete(image)
+        db.session.commit()
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
